22292e6f30d7d20f2ec59156e3ef31aeb9c83e4b
Sushil Kumar Gupta <sgupta7747@altimetrik.com>
2023-11-23 22:43:34 +0530

    1st commit

---
+++ src/test/java/org/example/AWSLearning.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class AWSLearning {
+
+ //   @Test
+    public void doTest()
+    {
+        System.out.print("this is aws");
+    }
+    public static void main(String... args)
+    {
+        System.out.print("this is aws");
+    }
+}
